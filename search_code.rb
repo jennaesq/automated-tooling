5c1b77aaf6cda446acaba6d1c52919b24fdab49b
Jenna Esquivel <jenna@puppet.com>
2018-02-06 11:24:57 -0800

    This change removes unnecessary iterations and remarks.

--- search_code.rb
+++ search_code.rb
@@ -52,14 +52,8 @@ options[:repo_regex] = '.*' if options[:repo_regex].nil?
 util = OctokitUtils.new(options[:oauth])
 
 #utilizing client.organization_repositories octokit method
-#repos = util.list_repos(options[:namespace], options)
-#repos.each do |r|
-#  puts "repos----> #{r}"
-#end
-
-search_results = []
-
-repos = [ 'puppetlabs-accounts', 'puppetlabs-modules']
+#repos = [ 'puppetlabs-accounts', 'puppetlabs-modules']
+repos = util.list_repos(options[:namespace], options)
 
 if repos.empty?
   puts "Exiting #{__FILE__}: No repos found for ${options[:namespace]}"
@@ -68,61 +62,42 @@ else
   repos.each do |r| #BEGIN repos loop
     #utilizing client.search_code octokit method
     query = "#{options[:search]} in:file repo:#{options[:namespace]}/#{r}"
-    #HELP ME!  Hash works for the first level, not seeming to work for lower levels.
-    results = util.search_code(r, query, options)
-    puts "Repo: #{r}, Query: #{query}"
+    puts "\nProcessing Repo: #{r}, Query: #{query}"
 
-    #Looks like we can't specify the damn branch, only fork=true; Lesson learned $#^@!
+    results ||= (util.search_code(r, query, options)).to_h
+    items   ||= results[:items]
+
+    #Looks like we can't specify the dang branch, only fork=true; Need to investigate
+    #octokit/search_code restrictions, and blow right past them.
     #utilizing client.branches octokit method
     #branches = util.list_branches(options[:namespace],r, options)
-    #branches.each do |b|
-    #  puts "branches------> #{b}"
-    #end
 
     #Are the results good, is there anything to work with?
     if results.count < 3
-      puts "Skipping #{r}: expecting 3 returned #{results.count}"
+      puts "\t...skipping #{r}: expecting 3 returned #{results.count}"
       next #Next repo
     elsif results[:incomplete_results] == "false"
-      puts "Skipping #{r}: 'incomplete_results' for #{query}"
+      puts "\t...skipping #{r}: 'incomplete_results' for #{query}"
       next #Next repo
     elsif results[:total_count] == "0"
-      puts "Skipping #{r}: 'total_count' is 0 for #{query}"
+      puts "\t...skipping #{r}: 'total_count' is 0 for #{query}"
       next #Next repo
-    elsif results[:items].empty?
-      puts "Skipping #{r}: no results found for #{query}"
+    elsif items.empty?
+      puts "\t...skipping #{r}: no results found for #{query}"
       next #Next repo
     end
 
-    #collect path from results
-    #HELP ME! Collect works.  How would this line return or build a hash, if results isn't a hash?
-    details  ||= results[:items].collect {|d| "#{d[:path]}, #{d[:html_url]}, #{d[:sha]}" }
-
-    if details.empty?
-      puts "no 'path' results found for #{query}"
-      next #Next repo
-    else
-      #Process the details (paths, in this case)
-      details.each do |path,url,sha|
-        row = {"repo" => r, "path" => path, "url" => url, "sha" => sha}
-        search_results.push(row)
-        #HELP ME!  This fine example sucks because it has only populated r and path.
-        #(Refer to HELP ME, line 100-ish)
-        puts "ROW: #{r},#{path},#{url},#{sha}"
-      end
-    end #END details empty? loop
-
+    #collect path,html_url,sha from results
+    #Hash[results[:items].collect {|d| [d[:path], "#{d[:html_url]}, #{d[:sha]}"]}]
 
     #Create a csv file, for ease of use.
-    CSV.open("search_code_#{options[:search]}.csv", "w") do |csv|
+    CSV.open("results_#{options[:search]}.csv", "w") do |csv|
       csv << ["repo", "path", "url", "sha"]
-      search_results.each do |s|
-        csv << [s["repo"], s["path"], s["url"], s["sha"]]
+      items.each do |i|
+        csv << [r, i[:path], i[:html_url], i[:sha]]
       end
-
     end #END csv
 
   end #END repos loop
 end # END repos.empty? loop
-
 
